3074d8974eaef722d42156a94d3834ca7b7f082f
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-07-24 19:04:02 -0400

    Showing Current Temp

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-alert("");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#degrees");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "3e2ce69ce76e734e0f6e4dbc39c85c56";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
