1cc6ca3d8288b682c88f91ae4aec5fcbe2cffef4
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-08-03 14:04:25 -0400

    Mission Accomplished!

--- src/script.js
+++ src/script.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   let apiKey = "91353889e1fe50ca6a1357473840b223";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -92,7 +92,7 @@ function displayTemperature(response) {
 
   iconElement.setAttribute(
     "src",
-    `http://api.openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
